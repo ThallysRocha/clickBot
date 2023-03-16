5ca5fa36a2e7be514b10a6541310eb44261ff06c
Thallys Rocha <thallyschrist@gmail.com>
2023-03-16 11:22:01 -0300

    v1.0

--- clickBot.py
+++ clickBot.py
@@ -4,7 +4,7 @@ import keyboard
 
 def escolherDelay(betweenClicks):
     try:
-        while int(betweenClicks) <= 0:
+        while float(betweenClicks) < 0:
             betweenClicks = pyautogui.prompt('Escolha um valor válido')
             return escolherDelay(betweenClicks)
         return betweenClicks
@@ -12,35 +12,31 @@ def escolherDelay(betweenClicks):
        betweenClicks = pyautogui.prompt('Escolha um valor válido')
        return escolherDelay(betweenClicks)
     
-def clicar(hotkey,running):
-    #print (running)
-    if running:
-        pyautogui.click(pyautogui.position())
-        startTime = time.time()
-        while keyboard.is_pressed(hotkey) == False:
-            if keyboard.is_pressed('esc') == True:
-                break
-            currentTime = time.time()
-            if (currentTime - startTime >= betweenClicks):
-                pyautogui.click(pyautogui.position())
-                startTime = time.time()
-        return
-    else:
-        time.sleep(1)
-        return
+def clicar(hotkey):
+    time.sleep(.5)
+    pyautogui.click(pyautogui.position())
+    startTime = time.time()
+    while keyboard.is_pressed(hotkey) == False:
+        if keyboard.is_pressed('esc') == True:
+            return
+        currentTime = time.time()
+        if (currentTime - startTime >= betweenClicks):
+            pyautogui.click(pyautogui.position())
+            startTime = time.time()
+    print("Parou...")        
+    time.sleep(.5)
+    return
 
 betweenClicks = pyautogui.confirm('Escolha o espaço entre cliques em segundos', buttons=['1', '2', '3','4','5','personalizado...'])
-if betweenClicks == 'personalizado...':
+if betweenClicks == 'personalizado...' or betweenClicks == None:
     betweenClicks = escolherDelay(betweenClicks)
 pyautogui.alert("Escolha a tecla para ativar/desativar clique.\n (Pressione uma tecla após o 'OK' ")
 hotkey = keyboard.read_key()
 pyautogui.alert("Pronto!\nPara começar feche o alerta e clique '"+hotkey+"'")
-betweenClicks = int(betweenClicks)
-running = False
-while keyboard.is_pressed('esc') == False:    
-    if keyboard.is_pressed(hotkey) == True:
-        running = not running
-        clicar(hotkey,running)
-        
-
+betweenClicks = float(betweenClicks)
+while keyboard.is_pressed('esc') == False:
+    if keyboard.is_pressed(hotkey) == True:        
+        print("Clicando...")
+        clicar(hotkey)       
+print("Fechou...") 
 pyautogui.alert("Programa fechado.")
